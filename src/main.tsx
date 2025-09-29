d9e47dd6b26f78ce7a9b36bcd7b93f11e1cecbef
ISQI\se.karimi <setykarimi80@gmail.com>
2025-09-29 14:24:58 +0330

    🌿feat: Edit for routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router'
+import  router from './routes.ts'
+
+
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider  router={router} />
 )
