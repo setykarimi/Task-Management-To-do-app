b864350d612dc9def9e3499b4009428334796a39
ISQI\se.karimi <setykarimi80@gmail.com>
2025-09-29 16:18:05 +0330

    🌿feat: Call api in onSubmit handle

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router'
 import './index.css'
 import router from './routes.ts'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
-  <RouterProvider  router={router} />
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider  router={router} />
+  </QueryClientProvider>
 )
--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,26 +1,44 @@
-import { useForm, type SubmitHandler } from "react-hook-form"
+import http from "@/lib/axios";
+import { useMutation } from "@tanstack/react-query";
+import { useForm, type SubmitHandler } from "react-hook-form";
 
 type Inputs = {
-  email: string
-  password: string
-}
+  email: string;
+  password: string;
+};
 
 export default function Login() {
-   const {
+  const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
-  } = useForm<Inputs>()
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    const result = await mutation.mutateAsync(data);
+    console.log(result)
+  };
+
+  const mutation = useMutation({
+    mutationFn: async (data: Inputs) => {
+      const res = await http.post("/auth/v1/signup", data);
+      return res.data;
+    },
+    onSuccess: (data) => {
+      console.log("Signup success:", data);
+    },
+    onError: (error:any) => {
+      console.error("Signup error:", error?.error_code);
+    },
+  });
 
   return (
-     <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <input defaultValue="test" {...register("email")} />
 
       <input type="password" {...register("password", { required: true })} />
 
       <button type="submit">Submit</button>
     </form>
-  )
+  );
 }
