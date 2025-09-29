2ebbf568b94676b5aced2fd0ca00839de35c3a61
ISQI\se.karimi <setykarimi80@gmail.com>
2025-09-29 15:28:55 +0330

    🌿feat: Install react hook form

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import { useForm, type SubmitHandler } from "react-hook-form"
+
+type Inputs = {
+  email: string
+  password: string
+}
 
 export default function Login() {
+   const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>()
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+
   return (
-    <div>Login</div>
+     <form onSubmit={handleSubmit(onSubmit)}>
+      <input defaultValue="test" {...register("email")} />
+
+      <input type="password" {...register("password", { required: true })} />
+
+      <button type="submit">Submit</button>
+    </form>
   )
 }
