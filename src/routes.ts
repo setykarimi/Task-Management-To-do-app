d3c6a9bf1adb88bc7c85839ccf8e80d0d1051052
ISQI\se.karimi <setykarimi80@gmail.com>
2025-09-30 12:19:09 +0330

    🌿feat: Add layout for routes

--- src/routes.ts
+++ src/routes.ts
@@ -2,20 +2,23 @@ import { createBrowserRouter } from "react-router";
 import { AddGroupTask, AddTask, Calender, Home, MyInfo } from "@/pages";
 import NotFound from "./pages/main/not-found";
 import Login from "./pages/auth/login";
+import Layout from "@/components/layout";
 
 
 export default createBrowserRouter([
   {
     path: "/",
+    Component: Layout,
     children: [
       { index: true, Component: Home },
       { path: "calender", Component: Calender },
       { path: "add-task", Component: AddTask },
       { path: "add-group-task" , Component: AddGroupTask},
-      { path: "my-info", Component: MyInfo }
+      { path: "my-info", Component: MyInfo },
+      { path: "login", Component: Login },
+      { path: "*", Component: NotFound }
     ],
   },
-  { path: "login", Component: Login },
-  { path: "*", Component: NotFound }
+ 
 ]);
 
