a4ea7b1ecdff967967bab9ec3b0894dd25fc5afc
ISQI\se.karimi <setykarimi80@gmail.com>
2025-10-01 11:13:53 +0330

    🌿feat: Add Toaster in layout

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,8 +1,10 @@
+import { Toaster } from 'react-hot-toast';
 import { Outlet } from "react-router";
 
 export default function Layout() {
   return (
     <div className={`bg-[url(/src/assets/img/background.png)] bg-no-repeat bg-cover`}>
+      <Toaster />
       <Outlet />
     </div>
   );
