15671d697effea58e694cd1b756064d82b2044cb
ISQI\se.karimi <setykarimi80@gmail.com>
2025-10-01 11:13:10 +0330

    🌿feat: Edit for password input

--- src/components/form/input.tsx
+++ src/components/form/input.tsx
@@ -1,4 +1,5 @@
-import type { FC } from "react"
+import { Eye, EyeSlash } from "iconsax-reactjs"
+import { useState, type FC } from "react"
 
 interface IProps{
     register: any, 
@@ -9,10 +10,16 @@ interface IProps{
 }
 
 const Input:FC<IProps> = ({register, label, name, type, rules})=> {
+  const [showPass, setShowPass] = useState(false)
+  const isPassword = type == "password"
+
   return (
     <div className="bg-white p-3 rounded-xl flex flex-col shadow">
         <label className="text-[#6E6A7C] text-xs mb-1">{label}</label>
-        <input className="outline-0 text-xs" type={type} {...register(name, rules)} />
+        <div className="flex gap-1">
+          <input className="outline-0 text-xs w-full" type={isPassword && showPass ? "text" : type} {...register(name, rules)} />
+          {isPassword && <button type="button" className="cursor-pointer" onClick={()=>setShowPass(!showPass)}> {showPass ? <Eye size="16" color="#6E6A7C"/> : <EyeSlash size="16" color="#6E6A7C"/> }</button>}    
+        </div>
     </div>
   )
 }
