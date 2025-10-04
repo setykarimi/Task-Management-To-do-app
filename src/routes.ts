1c17c94c5e040db561d9bc5b939e7b941015dca3
ISQI\se.karimi <setykarimi80@gmail.com>
2025-10-04 10:36:24 +0330

    🌿feat: New Routes for dashboard

--- src/routes.ts
+++ src/routes.ts
@@ -1,24 +1,24 @@
-import { createBrowserRouter } from "react-router";
-import { AddGroupTask, AddTask, Calender, Home, MyInfo } from "@/pages";
-import NotFound from "./pages/main/not-found";
-import Login from "./pages/auth/login";
 import Layout from "@/components/layout";
-
+import { AddGroupTask, AddTask, Calender, Home, MyInfo } from "@/pages";
+import { createBrowserRouter } from "react-router";
+import Login from "./pages/auth/login";
+import Dashboard from "./pages/main/dashboard";
+import NotFound from "./pages/main/not-found";
 
 export default createBrowserRouter([
   {
     path: "/",
     Component: Layout,
     children: [
-      { index: true, Component: Home },
+      { index: true, Component: Login },
+
+      { index: false, Component: Home },
+      { path: "dashboard", Component: Dashboard },
       { path: "calender", Component: Calender },
       { path: "add-task", Component: AddTask },
-      { path: "add-group-task" , Component: AddGroupTask},
+      { path: "add-group-task", Component: AddGroupTask },
       { path: "my-info", Component: MyInfo },
-      { path: "login", Component: Login },
-      { path: "*", Component: NotFound }
+      { path: "*", Component: NotFound },
     ],
   },
- 
 ]);
-
