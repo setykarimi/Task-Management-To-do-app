d3df86373342adcea2329d391ca5b6830a40c23f
ISQI\se.karimi <setykarimi80@gmail.com>
2025-10-05 09:30:57 +0330

    🌿feat: New api for TASKS_API

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -3,3 +3,7 @@ export const AUTH_API = {
     LOIGN: 'auth/v1/token?grant_type=password',
     REFRESH_TOKEN: 'auth/v1/token?grant_type=refresh_token'
 }
+
+export const TASKS_API = {
+    TASKS: 'rest/v1/tasks'
+}
