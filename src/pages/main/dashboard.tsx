c76c3f42ffdcf25bfc5c4f4cc18b42fbce33890e
ISQI\se.karimi <setykarimi80@gmail.com>
2025-10-05 09:30:38 +0330

    🌿feat: Fetch tasks in dashboard

--- src/pages/main/dashboard.tsx
+++ src/pages/main/dashboard.tsx
@@ -1,3 +1,28 @@
+import http from "@/lib/axios";
+import { TASKS_API } from "@/services/api";
+import { useMutation } from "@tanstack/react-query";
+import { useEffect } from "react";
+import toast from "react-hot-toast";
+
 export default function Dashboard() {
+
+  const { mutateAsync: fetchTasks, isPending } = useMutation({
+    mutationFn: async () => {
+      const res = await http.get(TASKS_API.TASKS);
+      return res.data;
+    },
+    onSuccess: (result) => { 
+      return result
+    },
+    onError: (error: any) => {
+      toast.error(error?.response?.data?.error_code);
+    },
+  });
+
+
+  useEffect(()=> {
+    fetchTasks()
+  },[])
+
   return <div>Dashboard</div>;
 }
