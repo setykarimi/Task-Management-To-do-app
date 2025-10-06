c2edc863c5fa63509a94008b12c3cec062898258
ISQI\se.karimi <setykarimi80@gmail.com>
2025-10-06 12:26:53 +0330

    🌿feat: Profile url

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,7 +1,8 @@
 export const AUTH_API = {
     SIGNUP: 'auth/v1/signup',
     LOIGN: 'auth/v1/token?grant_type=password',
-    REFRESH_TOKEN: 'auth/v1/token?grant_type=refresh_token'
+    REFRESH_TOKEN: 'auth/v1/token?grant_type=refresh_token',
+    PROFILE: 'rest/v1/profiles',
 }
 
 export const TASKS_API = {
