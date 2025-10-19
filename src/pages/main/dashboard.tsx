48d550f08e1fd8692347109a370b326436cbcb97
ISQI\se.karimi <setykarimi80@gmail.com>
2025-10-19 09:11:49 +0330

    🌿feat: Show task group

--- src/pages/main/dashboard.tsx
+++ src/pages/main/dashboard.tsx
@@ -1,3 +1,4 @@
+import type { ITaskGroup } from "@/components/types";
 import http from "@/lib/axios";
 import { useAuth } from "@/providers";
 import { TASKS_API } from "@/services/api";
@@ -56,48 +57,56 @@ export default function Dashboard() {
 
   return (
     <div>
-    <header>
-      <div className="flex items-center mb-4">
-        {/* <img src="" alt="avatar" /> */}
-        <div>
-          <span className="text-sm">Hello!</span>
-          <h3 className="text-lg font-bold">{profile[0]?.name}</h3>
+      <header>
+        <div className="flex items-center mb-4">
+          {/* <img src="" alt="avatar" /> */}
+          <div>
+            <span className="text-sm">Hello!</span>
+            <h3 className="text-lg font-bold">{profile[0]?.name}</h3>
+          </div>
+          <Notification className="ml-auto" size="21" variant="Bold" />
         </div>
-        <Notification className="ml-auto" size="21" variant="Bold" />
+      </header>
+      <section>
+      <div className="flex items-center"> 
+        <h4 className="font-bold text-lg">In Progress</h4> 
+        <span className="text-[#5F33E1] bg-[#EEE9FF] text-sm w-5 h-5 flex justify-center items-center rounded-full ml-2">
+          {inprogressTasks.length}
+        </span>
       </div>
-    </header>
-    <section>
-     <div className="flex items-center"> 
-      <h4 className="font-bold text-lg">In Progress</h4> 
-      <span className="text-[#5F33E1] bg-[#EEE9FF] text-sm w-5 h-5 flex justify-center items-center rounded-full ml-2">
-        {inprogressTasks.length}
-      </span>
-     </div>
-      <div className="flex gap-2 flex-nowrap overflow-x-auto overflow-y-hidden mt-2 pb-2 scroll-hide touch-pan-x">
-        {inprogressTasks?.map((task: ITask) => (
-          <div
-            key={task.id}
-            className="bg-[#E7F3FF] p-4 rounded-xl w-56 flex-shrink-0"
-          >
-            <span className="text-[#6E6A7C] text-xs block">{task.group_name}</span>
-            <span className="block font-medium">{task.title}</span>
-            <div className="flex h-2.5 w-full overflow-hidden rounded-full bg-white mt-2">
-              <div className="h-full w-1/2 bg-[#0087FF] rounded-full"></div>
+        <div className="flex gap-2 flex-nowrap overflow-x-auto overflow-y-hidden mt-2 pb-2 scroll-hide touch-pan-x">
+          {inprogressTasks?.map((task: ITask) => (
+            <div
+              key={task.id}
+              className="bg-[#E7F3FF] p-4 rounded-xl w-56 flex-shrink-0"
+            >
+              <span className="text-[#6E6A7C] text-xs block">{task.group_name}</span>
+              <span className="block font-medium">{task.title}</span>
+              <div className="flex h-2.5 w-full overflow-hidden rounded-full bg-white mt-2">
+                <div className="h-full w-1/2 bg-[#0087FF] rounded-full"></div>
+              </div>
             </div>
+          ))}
+        </div>
+
+      </section>
+      <section>
+        <div className="flex items-center my-4"> 
+          <h4 className="font-bold text-lg">Task Groups</h4> 
+          <span className="text-[#5F33E1] bg-[#EEE9FF] text-sm w-5 h-5 flex justify-center items-center rounded-full ml-2">
+            {taskGroups.length}
+          </span>
+      </div> 
+      <div className="flex flex-col gap-4">
+        {taskGroups?.map((task: ITaskGroup) => (
+          <div className="bg-white p-4 rounded-xl shadow">
+            <span className="font-medium">{task.title}</span>
+            <br></br>
+            <span className="text-[#6E6A7C] text-xs">{task.task_count} Tasks</span>
           </div>
         ))}
       </div>
-
-    </section>
-      {/* {taskGroups?.map((task: ITask) => (
-        <div>{task.title}</div>
-      ))}
-      {tasks?.map((task: ITask) => (
-        <div>
-          {task.title}
-          {task.end_date}
-        </div>
-      ))} */}
+      </section>
     </div>
   );
 }
