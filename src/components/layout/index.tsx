7f53bab47e8a286fb28743efa2605a1f4eea86f8
ISQI\se.karimi <setykarimi80@gmail.com>
2025-10-20 13:13:19 +0330

    🌿feat: Main layout edited

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -7,7 +7,7 @@ export default function Layout() {
     <AuthProvider>
       <div className={`bg-[url(/src/assets/img/background.png)] bg-no-repeat bg-cover`}>
         <Toaster />
-        <div className='p-4 min-h-[90vh]'>
+        <div className='p-4 min-h-[100vh] flex flex-col justify-between'>
           <Outlet />
         </div>
       </div>
--- src/pages/auth/layout.tsx
+++ src/pages/auth/layout.tsx
@@ -3,22 +3,25 @@ import { Outlet } from "react-router";
 
 export default function AuthLayout() {
   return (
-    <div>
-      <img src={person} alt="person" className="mx-auto py-20" />
+    <>
+      <div>
+        <img src={person} alt="person" className="mx-auto" />
+      </div>
+      <div>
+        <h2 className="font-bold text-xl text-center leading-6">
+          Task Management & <br /> To-Do List
+        </h2>
 
-      <h2 className="font-bold text-xl text-center leading-6">
-        Task Management & <br /> To-Do List
-      </h2>
+        <p className="text-[#6E6A7C] text-center text-sm leading-4 mt-3">
+          This productive tool is designed to help
+          <br />
+          you better manage your task
+          <br />
+          project-wise conveniently!
+        </p>
 
-      <p className="text-[#6E6A7C] text-center text-sm leading-4 mt-3">
-        This productive tool is designed to help
-        <br />
-        you better manage your task
-        <br />
-        project-wise conveniently!
-      </p>
-
-      <Outlet />
-    </div>
+        <Outlet />
+      </div>
+    </>
   );
 }
