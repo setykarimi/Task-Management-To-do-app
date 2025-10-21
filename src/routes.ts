89602eb592f212f9b6c03a1f60409f611af5c5ec
ISQI\se.karimi <setykarimi80@gmail.com>
2025-10-21 11:39:57 +0330

    🌿feat: Change routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,11 @@
 import Layout from "@/components/layout";
-import { AddTask, AddTaskGroup, Calender, Profile } from "@/pages";
+import { AddTask, AddTaskGroup, Calender, EditTask, EditTaskGroup, Profile } from "@/pages";
 import { createBrowserRouter } from "react-router";
 import AuthLayout from "./pages/auth/layout";
-import Login from "./pages/auth/login";
 import Signup from "./pages/auth/signup";
 import Dashboard from "./pages/main/dashboard";
 import NotFound from "./pages/main/not-found";
+import Login from "./pages/auth/login";
 
 export default createBrowserRouter([
   {
@@ -24,8 +24,17 @@ export default createBrowserRouter([
 
       { path: "dashboard", Component: Dashboard },
       { path: "calender", Component: Calender },
-      { path: "add-task", Component: AddTask },
-      { path: "add-task-group", Component: AddTaskGroup },
+      { path: "task", 
+        children:[
+          {path: 'add' , Component: AddTask},
+          {path: 'edit/:id' , Component: EditTask}
+        ] },
+      { path: "task-group",
+        children: [
+          { path: 'add', Component: AddTaskGroup },
+          { path: 'edit/:id', Component: EditTaskGroup },
+        ]
+      },
       { path: "profile", Component: Profile },
       { path: "*", Component: NotFound },
     ],
