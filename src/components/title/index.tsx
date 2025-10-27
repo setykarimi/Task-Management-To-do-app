21c77db67f40ad006ebc94ea80fa3a25521e67f2
ISQI\se.karimi <setykarimi80@gmail.com>
2025-10-27 13:30:40 +0330

    🌿feat: New component for Title

---
+++ src/components/title/index.tsx
@@ -0,0 +1,19 @@
+import { ArrowSquareLeft } from "iconsax-reactjs";
+import React, { type FC } from "react";
+import { useNavigate } from "react-router";
+
+const PageTitle: FC<{ title: string, children?: React.ReactElement }> = ({ title, children }) => {
+  const navigate = useNavigate();
+  return (
+    <div className="relative">
+      <button className="absolute cursor-pointer" onClick={() => navigate(-1)}>
+        <ArrowSquareLeft size="32" variant="Bulk" />
+      </button>
+      <h1 className="text-center font-bold text-xl">{title}</h1>
+
+      {children}
+    </div>
+  );
+};
+
+export default PageTitle;
