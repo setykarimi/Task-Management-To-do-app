8707c793f19a8e3647c12947ed09381e845a4e6a
ISQI\se.karimi <setykarimi80@gmail.com>
2025-10-28 09:57:12 +0330

    🌿feat: colors and icons static data

--- src/assets/statics/index.ts
+++ src/assets/statics/index.ts
@@ -4,3 +4,19 @@ export const taskType = [
   { title: "In progress", id: "inprogress", color: "#FF7D53", bg: "#FFE9E1" },
 ];
 
+export const colors = [
+  {id: "#F478B8|#FFE4F2", title: "Pink"},
+  {id: "#9260F4|#EDE4FF", title: "Purple"},
+  {id: "#FF9142|#FFE6D4", title: "Orange"},
+  {id: "#FFD12E|#FFF6D4", title: "Yellow"},
+]
+
+export const icons = [
+  {id: "ProfileTick", title: "ProfileTick"},
+  {id: "Broom", title: "Broom"},
+  {id: "EmptyWallet", title: "EmptyWallet"},
+  {id: "Flash", title: "Flash"},
+  {id: "Heart", title: "Heart"},
+  {id: "MessageText1", title: "MessageText1"},
+  {id: "Note1", title: "Note1"},
+]
