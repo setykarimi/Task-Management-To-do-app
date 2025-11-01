aa147d1332e1b72cfed83cacd802e404a835b6b6
ISQI\se.karimi <setykarimi80@gmail.com>
2025-11-01 10:24:02 +0330

    🌿feat: Change for setting status text

--- src/components/status/empty-state/index.tsx
+++ src/components/status/empty-state/index.tsx
@@ -1,10 +1,14 @@
 import { BoxSearch } from "iconsax-reactjs";
+import type { FC } from "react";
 
-export default function EmptyState() {
+const EmptyState :FC<{status: string}> = ({status}) => {
   return (
     <div className="flex flex-col items-center justify-center mt-100">
         <BoxSearch size="40" color="#5F33E1"/>
-        <span className="block text-center mt-4 font-bold text-lg">No results found</span>
+        <span className="block text-center mt-4 font-bold text-lg">{status ? status : "No results found"}</span>
     </div>
   )
 }
+
+
+export default EmptyState
