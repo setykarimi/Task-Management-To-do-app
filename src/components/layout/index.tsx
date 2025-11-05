014091b83623c17c5934dd0ceed645569f515b77
ISQI\se.karimi <setykarimi80@gmail.com>
2025-11-05 11:34:46 +0330

    🌿feat: Check pathname for not showing the bottom navigation in auth pages

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,16 +1,18 @@
 import { AuthProvider } from '@/providers';
 import { Toaster } from 'react-hot-toast';
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import BottomNavigation from '../bottom-navigation';
 
 export default function Layout() {
+  const location = useLocation()
+  const showNavbar = location.pathname == "/" || location.pathname == "/signup"
   return (
     <AuthProvider>
       <div className={`bg-[url(/src/assets/img/background.png)] bg-no-repeat bg-cover`}>
         <Toaster />
-        <div className='p-4 min-h-[100vh] flex flex-col justify-between'>
+        <div className='md:p-4 p-6 min-h-[100vh] flex flex-col justify-between'>
           <Outlet />
-          <BottomNavigation />
+          {!showNavbar && <BottomNavigation />}
         </div>
       </div>
     </AuthProvider>
